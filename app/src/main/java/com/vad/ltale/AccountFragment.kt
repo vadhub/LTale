5f76f835bebe009755e27e4ce836b59ee78794be
vadim <gabderahmanov99@gmail.com>
2022-11-24 22:44:49 +0300

    test adapter

--- app/src/main/java/com/vad/ltale/AccountFragment.kt
+++ app/src/main/java/com/vad/ltale/AccountFragment.kt
@@ -7,7 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.vad.ltale.data.Record
+import java.net.URI
+import java.util.*
 
 
 class AccountFragment : Fragment() {
@@ -21,6 +26,17 @@ class AccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val buttonCreateRecord: FloatingActionButton = view.findViewById(R.id.createRecordButton)
+        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerItemRecords)
+        val listRecord = listOf(
+            Record(10, "hello", URI(""), Date(123424424), 1000),
+            Record(10, "hello1", URI(""), Date(123424424), 1000),
+            Record(10, "hello2", URI(""), Date(123424424), 1000),
+            Record(10, "hello3", URI(""), Date(123424424), 1000)
+            )
+        val adapter = RecordAdapter()
+        adapter.setRecords(listRecord)
+        recyclerView.layoutManager = LinearLayoutManager(view.context)
+        recyclerView.adapter = adapter
         buttonCreateRecord.setOnClickListener { view.findNavController().navigate(R.id.action_accountFragment_to_recordFragment) }
     }
 }
