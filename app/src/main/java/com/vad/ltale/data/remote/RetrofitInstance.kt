03ab10477bd3ab649ae5238953c58bc4e943466a
vadim <gabderahmanov99@gmail.com>
2023-01-03 13:56:57 +0300

    okhttp interceptor

--- app/src/main/java/com/vad/ltale/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/vad/ltale/data/remote/RetrofitInstance.kt
@@ -1,12 +1,27 @@
 package com.vad.ltale.data.remote
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class RetrofitInstance {
+
+    private val interceptor: HttpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val client: OkHttpClient =
+        OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(interceptor)
+            .build()
+
     private val retrofit by lazy {
         Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .baseUrl("http://10.0.2.2:8080/")
             .build()
     }
@@ -20,10 +35,6 @@ class RetrofitInstance {
     }
 
     val apiUpload: FileService by lazy {
-        retrofit.create(FileService::class.java)
-    }
-
-    val apiDownload: FileService by lazy {
         retrofit.create(FileService::class.java)
     }
 }
