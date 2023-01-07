f1b51946af84063574afb52205f6ef3e2868cf9a
vadim <gabderahmanov99@gmail.com>
2023-01-07 19:55:37 +0300

    basic auth interceptor

---
+++ app/src/main/java/com/vad/ltale/data/remote/BasicAuthInterceptor.kt
@@ -0,0 +1,17 @@
+package com.vad.ltale.data.remote
+
+import okhttp3.Credentials
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+
+class BasicAuthInterceptor(private val username: String, private val password: String): Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val credential: String = Credentials.basic(username, password)
+        val request: Request = chain.request()
+        val authenticatedRequest: Request = chain.request().newBuilder().header("Authorization", credential).build()
+        return chain.proceed(authenticatedRequest)
+
+    }
+}
