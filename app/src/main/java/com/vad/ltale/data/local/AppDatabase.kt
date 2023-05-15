487b97d4bc8affe56508c84ee2157985efdfcd7d
vadim <gabderahmanov99@gmail.com>
2023-05-15 22:31:30 +0300

    create database object

---
+++ app/src/main/java/com/vad/ltale/data/local/AppDatabase.kt
@@ -0,0 +1,33 @@
+package com.vad.ltale.data.local
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.vad.ltale.data.Audio
+
+@Database(entities = [Audio::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun audioDao(): AudioDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
+
+}
