a858fdce09a8c33a49d58a31eb71b1520ede61e8
vadim <gabderahmanov99@gmail.com>
2023-05-15 22:15:44 +0300

    entity

--- app/src/main/java/com/vad/ltale/data/Audio.kt
+++ app/src/main/java/com/vad/ltale/data/Audio.kt
@@ -1,21 +1,25 @@
 package com.vad.ltale.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "audio_table")
 data class Audio(
     @SerializedName("id")
     @Expose
+    @PrimaryKey
     val id: Long = -1,
 
     @SerializedName("uri")
     @Expose
+    @ColumnInfo(name = "uri")
     val uri: String = "",
 
     @SerializedName("duration")
     @Expose
-    val duration: Long,
-
-    var isPlay: Boolean = false
-
+    @ColumnInfo(name = "duration")
+    val duration: Long
 )
