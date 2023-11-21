e1cc3796c44b3daeec5b3830d3e1e38daab49db0
vadim <gabderahmanov99@gmail.com>
2023-11-21 13:58:25 +0300

    remove space

--- app/src/main/java/com/vad/ltale/model/TimeFormatter.kt
+++ app/src/main/java/com/vad/ltale/model/TimeFormatter.kt
@@ -6,7 +6,7 @@ class TimeFormatter {
     companion object {
         fun format(time: Long) =
             String.format(
-                "%02d : %02d",
+                "%02d:%02d",
                 TimeUnit.MILLISECONDS.toMinutes(time),
                 TimeUnit.MILLISECONDS.toSeconds(time) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(time))
